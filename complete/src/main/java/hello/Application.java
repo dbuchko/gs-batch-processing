4bfa31e7971fcc482dbb5c673f2373c42841d3a3
Daniel L. Buchko <dbuchko@pivotal.io>
2016-09-06 11:53:03 -0400

    Converting to run inside SCDF, attempting to change datasource to HSQLDB.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,9 +1,13 @@
 package hello;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 
 @SpringBootApplication
+@EnableTask
+@EnableBatchProcessing
 public class Application {
 
     public static void main(String[] args) throws Exception {
